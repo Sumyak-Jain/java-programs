3d26c00713adb2ed89de96f80c541e3144b50276
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-16 10:36:42 +0530

    java code for finding the largest of three

---
+++ sum.java
@@ -0,0 +1,11 @@
+//java code for finding the largest of three
+public class sum{
+public static void main(String args[]){
+	int a,b,c;
+  a=Integer.parseInt(args[0]);
+  b=Integer.parseInt(args[1]);
+ 
+
+System.out.println("the sum of two is"+ (a+b));
+}
+}
