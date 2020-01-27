d1c822136f7f8af8de2b1aec6d9c547fadfde8d8
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-28 00:23:03 +0530

    program to find the sum of all integers greater than 40 and less than 250

---
+++ divisible.java
@@ -0,0 +1,9 @@
+//program to find the sum of all integers greater than 40 and less than 250 that are divisible by 5.
+class divisible{
+public static void main(String args[]){
+int sum=0;
+for(int i=41;i<250;i++){
+if(i%5==0){
+sum=sum+i;}
+}
+System.out.println(sum);}}
