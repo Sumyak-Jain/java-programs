2637d95917528aa6c33b6ee81e61d612ab946751
SUMYAK JAIN <3456gaurav@gmail.com>
2020-04-03 23:23:10 +0530

    Create 1)thread1.java

---
+++ lab9/1)thread1.java
@@ -0,0 +1,10 @@
+public class thread1 extends Thread {
+     public void run(){
+        System.out.println("thread is running");//creating thread by extending thread class
+    }
+     public static void main(String[] args) {
+        thread1 t = new thread1();
+        t.start();
+    }
+    
+}
